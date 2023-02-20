9b5763420fdf940d188a14c002fa0144449dbb13
Vini Costa <vc896536@gmail.com>
2023-02-20 20:49:49 -0300

    Post entity

---
+++ src/main/java/com/example/springbootmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootmongo.repository;
+
+import com.example.springbootmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
