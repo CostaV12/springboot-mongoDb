ff3cebe8b0add4797abb95e68b9f366bd31a9a06
Vini Costa <vc896536@gmail.com>
2023-02-20 14:24:07 -0300

    Insert user

--- src/main/java/com/example/springbootmongo/resource/UserResource.java
+++ src/main/java/com/example/springbootmongo/resource/UserResource.java
@@ -6,7 +6,9 @@ import com.example.springbootmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,7 @@ public class UserResource {
     @GetMapping
     public ResponseEntity<List<UserDto>> findAll() {
 
-        List<User> users = new ArrayList<>(userService.userList());
+        List<User> users = new ArrayList<>(userService.findAll());
         List<UserDto> usersDto = users.stream().map(UserDto::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(usersDto);
     }
@@ -33,6 +35,14 @@ public class UserResource {
         User user = userService.findById(id);
 
         return ResponseEntity.ok().body(new UserDto(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDto userDto) {
+        User user = userService.fromDto(userDto);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/example/springbootmongo/service/UserService.java
+++ src/main/java/com/example/springbootmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.springbootmongo.service;
 
 import com.example.springbootmongo.domain.User;
+import com.example.springbootmongo.dto.UserDto;
 import com.example.springbootmongo.repository.UserRepository;
 import com.example.springbootmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,20 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> userList () {
+    public List<User> findAll () {
         return userRepository.findAll();
     }
 
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDto(UserDto userDto) {
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
 }
